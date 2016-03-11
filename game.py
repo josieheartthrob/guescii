ba32882450a79ac1e93ab3cc1f83cbad7e942472
josieheartthrob <josiahst@gmail.com>
2016-03-10 19:47:09 -0800

    designed and implemented some methods for the game class

--- game.py
+++ game.py
@@ -3,8 +3,15 @@ class Game(object):
 
     def __init__(self, settings):
         """Assumes settings is a settings dictionary."""
+        if type(settings) != dict:
+            raise TypeError
+        settings_keys = ("Guess-Types", "Combination Length", "Guesses")
+        if settings_keys not in settings.keys():
+            raise ValueError
+
         self.settings = settings
         self.answer = self.build_answer()
+        self.answer_map = {L: self.answer.count(L) for L in set(self.answer)}
 
     def build_answer(self):
         """Creates a randomized string of letters based off the settings."""
@@ -14,7 +21,45 @@ class Game(object):
             answer += self.settings["Guess Types"].choice()
         return answer
 
-    def ask_for_guess(self):
-        """Ask the user for their guess and wait for their input. If the input is invalid, ask again until it's valid"""
+    def compare_guess(self, guess):
+        """Assumes guess is a string as long as the combination length.
 
-        guess = raw_input("> ")
+        Return a string that gives the user info about their guess."""
+        if type(guess) != str:
+            raise TypeError
+        try:
+            for letter in set(guess):
+                assert letter in self.settings["Guess-Types"]
+            assert len(guess) == self.settings["Guess Length"]
+        except:
+            raise ValueError
+
+        total_similar_letters = sum(self.answer_map.itervalues())
+        correct_letters, similar_letters = (
+            self.differentiate_letters(total_similar_letters))
+        return self.parse_into_answer_info(correct_letters, similar_letters)
+
+    def differentiate_letters(self, guess, similar_letters):
+        """Assumes similar_letters is a positive integer;
+        Guess is a string as long as the combination length
+
+        Return the amount of similar letters and the ammount of correct letters in the guess."""
+        try:
+            assert type(guess) == str
+            assert type(similar_letters) == int
+        except:
+            raise TypeError
+        try:
+            for letter in set(guess):
+                assert letter in self.settings["Guess-Types"]
+            assert len(guess) == self.settings["Guess Length"]
+            assert similar_letters >= 0
+        except:
+            raise ValueError
+
+        correct_letters = 0
+        for i in xrange(len(guess)):
+            if guess[i] == self.answer[i]:
+                similar_letters -= 1
+                correct_letters += 1
+        return correct_letters, similar_letters
